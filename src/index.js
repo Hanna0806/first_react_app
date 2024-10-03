ed819f872a10869f9c53bf796a20b15c94864ebe
anna <anna.gal0806@gmail.com>
2024-10-03 21:16:17 +0300

    add react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import logo from "./logo192.png";
+import "./style.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const element = <div>Hello</div>;
+// root.render(element);
+// console.log(element)
+
+function Card(props) {
+    console.log(props);
+
+    return (
+        <div>
+            <img src={logo}></img>
+            <p>Description</p>
+        </div>
+    );
+}
+
+function Form() {
+    const printLog = (e) => {
+        e.prevetDefault();
+        console.log(e.target.value);
+        console.log("ONCHANGE");
+
+    }
+    return (
+        <div>
+             <form>
+            <input type='text' onChange={printLog} />
+           <Button />
+            </form>
+        </div>
+    )
+}
+
+function Button() {
+
+const printLog = (e) => {
+    e.prevetDefault();
+    console.log("click");
+};
+
+    return (<button onClick={printLog} className='btn2'>Button</button>);
+}
+
+function App(param) {
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <form>
+                <input type='text'></input>
+                <button>Button</button>
+            </form>
+            <Card />
+            <Form abracadabra="some value"/>
+        </div>
+    );
+}
+root.render(<App />);
+
+const elem = React.createElement(
+    "h1",
+    { className: "testClass" },
+    "Hello"
+)
+console.log(elem)
+
+
+
+// root.render(
+
+//     <App />
+
+// );
